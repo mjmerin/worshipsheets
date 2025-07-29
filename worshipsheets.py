8fbeacd4b361a80f849924d4ed01fca8211435a9
Mark John Merin <mjmerin@gmail.com>
2025-07-29 16:10:33 -0700

    multiple URL support.

--- worshipsheets.py
+++ worshipsheets.py
@@ -2,32 +2,37 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
+from queue import Queue
+
 # local libraries
 import lyrics_parser
 import txt_to_pdf
 import txt_to_docx
 
+text_queue = Queue(maxsize=0) 
+text_list = []
+
 # URL of the song lyrics page
-url = sys.argv[1]
+for arg in sys.argv[1:]:  # Slice the list to exclude sys.argv[0]
+    url = arg
 
-# Send a GET request to the website
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+    # Send a GET request to the website
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-# Find the main content div where lyrics are located
-content_div = soup.find('div', class_='song-chords-content') #worshipchords
+    # Find the main content div where lyrics are located
+    content_div = soup.find('div', class_='song-chords-content') #worshipchords
 
-# Extract and clean the text
-if content_div:
-    print(f"Extracting lyrics from {url}.")
-    text = content_div.get_text(separator='\n').strip()
-    stripped_text = lyrics_parser.strip_chords_from_lyrics(text)
-    txt_to_docx.advanced_text_to_docx(stripped_text, "lyrics.docx", "Lyrics", columns=2) 
-    
-else:
-    print(f"Could not find lyrics section on page: {url}.")
+    # Extract and clean the text
+    if content_div:
+        print(f"Extracting lyrics from {url}.")
+        text = content_div.get_text(separator='\n').strip()
+        stripped_text = lyrics_parser.strip_chords_from_lyrics(text)
+        text_queue.put(stripped_text)
+        text_list.append(stripped_text)
+    else:
+        print(f"Could not find lyrics section on page: {url}.")
 
-
-
+txt_to_docx.create_continuous_two_column_docx(*text_list)
 
 
