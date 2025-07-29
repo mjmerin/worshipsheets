c2d072a3e31299b01de5120db97aa1826e3bb71d
Mark John Merin <mjmerin@gmail.com>
2025-07-29 14:48:46 -0700

    make the appropriate calls.

--- worshipsheets.py
+++ worshipsheets.py
@@ -2,8 +2,10 @@ import requests
 import sys
 from bs4 import BeautifulSoup
 
+# local libraries
 import lyrics_parser
 import txt_to_pdf
+import txt_to_docx
 
 # URL of the song lyrics page
 url = sys.argv[1]
@@ -13,31 +15,17 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 # Find the main content div where lyrics are located
-# content_div = soup.find('div', class_='tabcontent') #pnwchords
-# content_div = soup.find('div', class_='page-wrap') #essentialworship
-# content_div = soup.find('div', class_='ct-div-block') #colloborateworship
-# content_div = soup.find('div', class_='lyrics-disp') #worshiptogether
 content_div = soup.find('div', class_='song-chords-content') #worshipchords
 
 # Extract and clean the text
 if content_div:
+    print(f"Extracting lyrics from {url}.")
     text = content_div.get_text(separator='\n').strip()
     stripped_text = lyrics_parser.strip_chords_from_lyrics(text)
-    txt_to_pdf.create_two_column_pdf(stripped_text, "test.pdf", "TEST")
+    txt_to_docx.advanced_text_to_docx(stripped_text, "lyrics.docx", "Lyrics", columns=2) 
     
-    # Find and extract the Lyrics
-    lines = stripped_text.split('\n')
-    lyrics_lines = []
-    for line in lines:
-            if line.strip() == "":
-                break  # End of Verse 1
-                lyrics_lines.append(line.strip())
-    
-    # Output the result
-    for line in lines:
-        print(line)
 else:
-    print("Could not find lyrics section on the page.")
+    print(f"Could not find lyrics section on page: {url}.")
 
 
 
