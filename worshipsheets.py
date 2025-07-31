65986c29fe6466db11d44860c366415a22fc79df
Mark John Merin <mjmerin@gmail.com>
2025-07-30 23:37:43 -0700

    use the new methods.

--- worshipsheets.py
+++ worshipsheets.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 
 # local libraries
 import lyrics_parser
+import title_parser
 import txt_to_docx
 
 text_list = []
@@ -11,6 +12,7 @@ text_list = []
 # URL of the song lyrics page
 for arg in sys.argv[1:]:  # Slice the list to exclude sys.argv[0]
     url = arg
+    song_title = title_parser.extract_song_title_from_url(url)
 
     # Send a GET request to the website
     response = requests.get(url)
@@ -21,9 +23,10 @@ for arg in sys.argv[1:]:  # Slice the list to exclude sys.argv[0]
 
     # Extract and clean the text
     if content_div:
-        print(f"Extracting lyrics from {url}.")
+        print(f"Extracting {song_title} lyrics from {url}.")
         text = content_div.get_text(separator='\n').strip()
         stripped_text = lyrics_parser.strip_chords_from_lyrics(text)
+        stripped_text = lyrics_parser.append_title(stripped_text, song_title)
         text_list.append(stripped_text)
     else:
         print(f"Could not find lyrics section on page: {url}.")
